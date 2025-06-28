64080804b37da9e3e1a1232893e6e1fdde6e730e
Džuris <juris@glaive.pro>
2025-06-28 14:09:25 +0300

    Update formatting

--- src/App.vue
+++ src/App.vue
@@ -3,47 +3,42 @@ import TheMap from './components/TheMap.vue'
 </script>
 
 <template>
-  <aside>
-    <section>
-      // Load/select dataset
-    </section>
+    <aside>
+        <section>// Load/select dataset</section>
 
-    <section>
-      // Select names (regexes) and adjust colors
-      // suggest names
-    </section>
-  </aside>
+        <section>// Select names (regexes) and adjust colors // suggest names</section>
+    </aside>
 
-  <main>
-    <TheMap />
-  </main>
+    <main>
+        <TheMap />
+    </main>
 </template>
 
 <style scoped>
 aside {
-  line-height: 1.5;
+    line-height: 1.5;
 }
 
 .logo {
-  display: block;
-  margin: 0 auto 2rem;
+    display: block;
+    margin: 0 auto 2rem;
 }
 
 @media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+    header {
+        display: flex;
+        place-items: center;
+        padding-right: calc(var(--section-gap) / 2);
+    }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+    .logo {
+        margin: 0 2rem 0 0;
+    }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+    header .wrapper {
+        display: flex;
+        place-items: flex-start;
+        flex-wrap: wrap;
+    }
 }
 </style>
--- src/components/TheMap.vue
+++ src/components/TheMap.vue
@@ -1,20 +1,20 @@
 <script setup lang="ts">
-import "leaflet/dist/leaflet.css"
-import { ref } from "vue"
-import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet"
+import 'leaflet/dist/leaflet.css'
+import { ref } from 'vue'
+import { LMap, LTileLayer } from '@vue-leaflet/vue-leaflet'
 
 const zoom = ref(2)
 </script>
 
 <template>
-  <div style="height:600px; width:800px">
-    <LMap ref="map" v-model:zoom="zoom" :center="[47.41322, -1.219482]">
-      <LTileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        layer-type="base"
-        name="OpenStreetMap"
-      ></LTileLayer>
-      <!--
+    <div style="height: 600px; width: 800px">
+        <LMap ref="map" v-model:zoom="zoom" :center="[47.41322, -1.219482]">
+            <LTileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                layer-type="base"
+                name="OpenStreetMap"
+            ></LTileLayer>
+            <!--
       'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
 	    {
 	      attribution: `&copy;<a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>,
@@ -23,6 +23,6 @@ const zoom = ref(2)
 	      maxZoom: 14,
 	    }
       -->
-    </LMap>
-  </div>
+        </LMap>
+    </div>
 </template>
