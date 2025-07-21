a3bd73606e5d5b4770777067f27cb665bdae9f56
Džuris <juris@glaive.pro>
2025-07-21 21:01:45 +0300

    Clean up for building

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+// @ts-expect-error broken types due to exports
 import eslint from 'vite-plugin-eslint'
 
 // https://vite.dev/config/
